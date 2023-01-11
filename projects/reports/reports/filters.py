cdbdb7f394c76527cb8d81094eafdab1f69b4b62
Pablo Castellanos <pablocastellanos@gmail.com>
2023-01-11 13:31:00 -0300

    feat(reports): Added validation to fields

--- projects/reports/reports/filters.py
+++ projects/reports/reports/filters.py
@@ -19,8 +19,8 @@ class _BaseQuery:
     date_before: datetime | None = Query(None, description="End date of reports")
     object: Pattern | None = Query(None, description="Reports for object IDs matching regex")
     owned: bool = Query(False, description="Whether to include only reports owned by requesting user")
-    page: int = Query(1, description="Page for paginated results")
-    page_size: int = Query(10, description="Number of reports per page")
+    page: int = Query(1, description="Page for paginated results", ge=1)
+    page_size: int = Query(10, description="Number of reports per page", ge=1)
     order_by: Literal[""] = Query("", description="NEEDS TO BE OVERRIDEN!!")
     direction: Literal["1", "-1"] = Query(-1, description="Sort by ascending or descending values")
 
