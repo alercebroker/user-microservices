254949be92e50033440e1c0891d1db545159ca3d
Pablo Castellanos <pablocastellanos@gmail.com>
2023-01-25 16:49:51 -0300

    refactor(libs): Explicit imports in __init__

--- libs/query/query/__init__.py
+++ libs/query/query/__init__.py
@@ -1,4 +1,4 @@
-from ._utils import *
+from ._utils import QueryRecipe, BaseQuery, BaseSortedQuery, BasePaginatedQuery, Direction, field_enum_factory
 
 
 __all__ = ["QueryRecipe", "BaseQuery", "BaseSortedQuery", "BasePaginatedQuery", "Direction", "field_enum_factory"]
