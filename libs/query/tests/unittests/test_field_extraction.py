dd1e92d34b9f4cf8ff043ff5cb7bd04e1427e34e
Pablo Castellanos <pablocastellanos@gmail.com>
2023-01-26 12:04:22 -0300

    tests(query): Updated tests

---
+++ libs/query/tests/unittests/test_field_extraction.py
@@ -0,0 +1,31 @@
+import pytest
+from pydantic import BaseModel, Field
+
+from query import fields
+
+
+def test_field_enum_by_alias():
+    class MockModel(BaseModel):
+        field1: str = Field(..., alias="alias1")
+        field2: str
+
+    output_enum = fields(MockModel, by_alias=True)
+    assert output_enum == ("alias1", "field2")
+
+
+def test_field_enum_by_name():
+    class MockModel(BaseModel):
+        field1: str = Field(..., alias="alias1")
+        field2: str
+
+    output_enum = fields(MockModel, by_alias=False)
+    assert output_enum == ("field1", "field2")
+
+
+def test_field_enum_with_exclusion():
+    class MockModel(BaseModel):
+        field1: str = Field(..., alias="alias1")
+        field2: str
+
+    output_enum = fields(MockModel, exclude={"field1"})
+    assert output_enum == ("field2",)
