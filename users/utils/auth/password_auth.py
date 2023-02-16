25c568bb4327a21668b6cb951d78b3a7e7fe50aa
Alex Alvarez Toledo <otajla@hotmail.com>
2023-02-16 15:03:23 -0300

    refactor: password auth client completed. General Fixes

--- users/utils/auth/password_auth.py
+++ users/utils/auth/password_auth.py
@@ -1,18 +1,17 @@
-from cryptography.fernet import Fernet
+
 from bcrypt import gensalt, hashpw, checkpw
 
 class PasswordAuthClient(object):
     
     def __init__(self, settings) -> None:
-        self.settings = settings
-        self.fernet = Fernet(self.settings.secret_key)
+        pass
 
-    def generate_new_password_hash(self, password_string):
-        encrypted_password = self.fernet.encrypt(password_string)
-        hashed_password = hashpw(encrypted_password, gensalt())
+    
+    def generate_new_password_hash(self, password_string: str):
+        salt = gensalt()
+        hashed_password = hashpw(password_string.encode(), salt)
         return hashed_password
 
-    def validate_password(self, password_string, stored_password):
-        encrypted_password_input = self.fernet.encrypt(password_string)
-        match = checkpw(encrypted_password_input, stored_password)
+    def validate_password(self, password_string: str, stored_password: str):
+        match = checkpw(password_string.encode(), stored_password)
         return match
